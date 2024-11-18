8131f0d18575a31b0282d83b85cf63dbcc5d7be9
dmojzes <imdanielmojzes@gmail.com>
2024-11-18 13:09:20 -0500

    test_runner.py done

--- test_runner.py
+++ test_runner.py
@@ -9,6 +9,7 @@ def runCommand(command):
 
 def main():
     timeArr = []
+    transferRate = []
 
     if len(sys.argv)<2:
         print("TOO SMALL")
@@ -27,20 +28,31 @@ def main():
             runCommand(localCommand)
             endTime = time.time()
             timeArr.append(endTime - startTime)
+            transferRate.append(69855/(endTime - startTime))
         elif (sys.argv[1]=="Server"):
             localCommand = "./sendFile " + LocalArgs
             startTime = time.time()
             runCommand(localCommand)
             endTime = time.time()
             timeArr.append(endTime - startTime)
+            transferRate.append(69855/(endTime - startTime))
 
     sum = 0
     for i in timeArr:
         sum += i
 
+    transferSum = 0
+    for i in transferRate:
+        transferSum += i
+        
+    print()
     print("Min time: " + str(min(timeArr)))
     print("Average time: " + str(sum/len(timeArr)))
     print("Max time: " + str(max(timeArr)))
+    print()
+    print("Min transfer rate: " + str(min(transferRate)))
+    print("Average transfer rate: " + str(transferSum/len(transferRate)))
+    print("Max transfer rate: " + str(max(transferRate)))
 
 if __name__ == "__main__":
     main()
